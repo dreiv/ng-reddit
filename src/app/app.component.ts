06f595ad355c5cf08e9d69e9acfb8aa55e184ae6
dreiv <dreivoicu@gmail.com>
2017-12-24 12:22:14 +0200

    preety much done with this par

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,4 +27,8 @@ export class AppComponent {
     linkEl.value = '';
     return false;
   }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
